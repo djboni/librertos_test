9ba102d64141033622226edd4fcc7ee3a387418e
Djones A. Boni <djboni@gmail.com>
2016-12-10 12:07:12 -0200

    misc: Move misc code to main file

--- TestA/tests/main.cpp
+++ TestA/tests/main.cpp
@@ -1,3 +1,28 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
+
+#include "projdefs.h"
+
+/* ASSERT */
+
+void myassert(int x)
+{
+    if(x == 0)
+        throw 0;
+}
+
+/* CONCURRENT ACCESS */
+
+static void(*func)(void) = (void(*)(void))0;
+
+void librertos_test_set_concurrent_behavior(void(*f)(void))
+{
+    func = f;
+}
+
+void librertos_test_concurrent_access(void)
+{
+    if(func != (void(*)(void))0)
+        func();
+}
--- TestA/tests/myassert.cpp
+++
@@ -1,7 +0,0 @@
-#include "projdefs.h"
-
-void myassert(int x)
-{
-    if(x == 0)
-        throw 0;
-}
